06551efef32322d9135ce818fa30c4631c51398a
Ajae Obaze <72343143+ajpharrelo@users.noreply.github.com>
2022-11-12 08:45:34 +0000

    Daily commit

--- 2FA_RFID/src/main.cpp
+++ 2FA_RFID/src/main.cpp
@@ -51,26 +51,12 @@ void loop()
   //Show UID on serial monitor
   Serial.print("UID tag :");
   String content= "";
-  byte letter;
+
   for (byte i = 0; i < mfrc522.uid.size; i++) 
   {
      Serial.print(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " ");
      Serial.print(mfrc522.uid.uidByte[i], HEX);
      content.concat(String(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " "));
      content.concat(String(mfrc522.uid.uidByte[i], HEX));
-  }
-  Serial.println();
-  Serial.print("Message : ");
-  content.toUpperCase();
-  if (content.substring(1) == "F7 B1 B2 4D") //change here the UID of the card/cards that you want to give access
-  {
-    Serial.println("Authorized access");
-    Serial.println();
-    delay(3000);
-  }
- 
- else   {
-    Serial.println(" Access denied");
-    delay(3000);
   }
 } 
