e77b36b3753544c445cbab0920579f7fa5acd3ce
Ajae Obaze <72343143+ajpharrelo@users.noreply.github.com>
2022-11-20 13:06:31 +0000

    Removed FIXME in client.js

--- client/client.js
+++ client/client.js
@@ -14,10 +14,6 @@ let client = net.createConnection({host: "192.168.0.40", port: 80}, () => {
     client.write("READ");
 });
 
-// FIXME:
-// checkID returning false when using data sent from ESP32 
-// but returns true when using literal value
-
 client.on('data', (data) => {
     if(checkID(data.toString()) == true)
     {
