923727452de511dd1907cc7de9a974cbed83de94
Ajae Obaze <72343143+ajpharrelo@users.noreply.github.com>
2023-06-26 15:20:20 +0100

    Added bare minimum functionality code to Auth class

--- Desktop/Authentication/Logic/Auth.cs
+++ Desktop/Authentication/Logic/Auth.cs
@@ -4,6 +4,64 @@ namespace Authentication.Logic
 {
     public class Auth
     {
+        private SerialPort _port = new SerialPort();
+        private bool portValid = false;
+        public Auth(string portName, int baud)
+        {
+            _port.PortName = portName;
+            _port.BaudRate = baud;
+        }
 
+        public void Connect()
+        {
+            try
+            {
+                _port.Open();
+                portValid = true;
+            }
+            catch (Exception)
+            {
+                portValid = false;
+            }
+        }
+
+        public void Disconnect()
+        {
+            if(_port.IsOpen)
+            {
+                _port.Close();
+                portValid = false;
+            }
+            else
+            {
+                throw new Exception("Not currently connected to port");
+            }
+        }
+
+        public long GetAuthID()
+        {
+            if (portValid)
+            {
+                long id;
+                try
+                {
+                    id = long.Parse(_port.ReadLine());
+                    return id;
+                }
+                catch (Exception)
+                {
+                    throw new Exception("Data sent from port invalid");
+                }
+            }
+            else
+            {
+                throw new Exception("Could not verify connection to port");
+            }
+        }
+
+        public bool PortValid()
+        {
+            return portValid;
+        }
     }
 }
