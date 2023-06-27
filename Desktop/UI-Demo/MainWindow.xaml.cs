a6d81be256d4272c257398a7857ef2e8cd196aa3
Ajae Obaze <72343143+ajpharrelo@users.noreply.github.com>
2023-06-27 15:28:55 +0100

    Added User class to UI Demo

--- Desktop/UI-Demo/MainWindow.xaml.cs
+++ Desktop/UI-Demo/MainWindow.xaml.cs
@@ -1,7 +1,25 @@
-﻿using Wpf.Ui.Controls;
+﻿using System.Windows;
+using Authentication.Logic;
+using System.Threading;
+using WPFUI = Wpf.Ui.Controls;
+using System.Threading.Tasks;
 
 namespace UI_Demo
 {
+    public class User
+    { 
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public long CardID { get; set; }
+
+        public User(string user, string pass, long cardID)
+        {
+            Username = user;
+            Password = pass;
+            CardID = cardID;
+        }
+    }
+
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
